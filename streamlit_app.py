3a0ad366c27eacc5dffd3b5abc8957233c8f7a19
notlaeel <notlaeel@gmail.com>
2024-11-17 02:07:01 +0000

    testing secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@ import streamlit as st
 
 
 st.title("🎈 My new streamlit app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.write(st.secrets["altonkey"])
 
 print(st.secrets["altonkey"])
 
